8fe97c09e4a408a9cc3c5c82773be6c42e0456d6
Gladyston-Estevao <167896922+Gladyston-Estevao@users.noreply.github.com>
2025-02-05 13:35:22 -0300

    Att Logica de Programação

---
+++ HashtagTreinanamentos-main/Logica de Programação/log_if.py
@@ -0,0 +1,13 @@
+#Código para definir qual bônus o funcionário deverá receber se alcançar a quantia de meta, utilizando Estrutura Condicional.
+
+vendas = 1200
+meta = 1000
+
+if vendas > meta:
+    bonus = 250
+else:
+    bonus = 50
+
+print(bonus)
+
+
